00d549ba50d57ca0f59ef60c0175c6fbe8ebdc33
Aniko Kopis-Horvath <ha891209@gmail.com>
2021-01-18 16:01:48 +0100

    2nd phone size is not a fit yet

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,12 @@
-const menuButton = document.querySelector('.nav-button');
-menuButton.addEventListener('click', () => {
-    const navList = document.querySelector('.nav-list');
-    if (navList.style.display == 'none')
-        navList.style.display = 'block';
-    else
-        navList.style.display = 'none';
-});
+'use strict';
 
-const navHide = () => {
-    const navList = document.querySelectorAll('.nav-link');
-    navList.forEach(link => link.addEventListener('click', () => {
-        const navList = document.querySelector('.nav-list');
-        if (navList.style.display == 'none')
-            navList.style.display = 'block';
-        else
-            navList.style.display = 'none';
-    }));
-}
-
-navHide();
+const menu = document.querySelector('.nav');
+window.onscroll = function () {
+    'use strict';
+    if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20) {
+        menu.classList.add("white-bg");
+    }
+    else {
+        menu.classList.remove("white-bg");
+    }
+};
